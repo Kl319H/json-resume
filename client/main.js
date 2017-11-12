c1de7c39078d8cc2941f604482af37ef2cb5d3d1
kl319h <kleigh.sanchez@gmail.com>
2017-11-11 19:13:04 -0700

    refactor json resume meteor project for future wip

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { resume } from './resume.js';
+import './dark-classy-responsive.css';
+import './main.html';
+

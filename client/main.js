a40d36a4e20bdafae20d380a8d0ff9c31f7f1b64
kl319h <kleigh.sanchez@gmail.com>
2018-03-11 20:15:44 -0600

    cleanup

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { resume } from './resume.js';
 import './dark-classy-responsive.css';
 import './main.html';
 
